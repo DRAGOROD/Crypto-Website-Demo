43185cc745e21dc4964603d968330ae3a85005c7
dragorod <jitmishra40@gmail.com>
2026-06-10 01:38:51 +0530

    searchbox fix

--- crypto-app/src/homepage.jsx
+++ crypto-app/src/homepage.jsx
@@ -7,8 +7,8 @@ function homepage(){
             <div id="search-container">
                 <h3 id="search-heading">Look for the Best Performing Coins</h3>
                 <form id="search-box">
-                    <input type="text" placeholder="Search Crypto..." id="search-input-box"/>
-                    <button type="submit" id="search-btn">Search</button>
+                    <input type="text" placeholder="Search Crypto........" id="search-input-box"/>
+                    <button type="submit" id="search-btn">&#x2315;</button>
                 </form>
             </div>
         </div>
